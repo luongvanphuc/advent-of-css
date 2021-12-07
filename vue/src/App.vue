1652d4c66033d00fb188cb7759662b1ce5baed03
Phuc Luong <phuc@inspectorio.com>
2021-12-07 16:12:46 +0700

    up

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Day 1</router-link> |
+    <router-link to="/day-2">Day 2</router-link>
   </div>
   <router-view />
 </template>
--- vue/src/router/index.ts
+++ vue/src/router/index.ts
@@ -1,20 +1,20 @@
-import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
+import Day1 from '../pages/Day-1.vue';
 
 const routes: Array<RouteRecordRaw> = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    name: 'Day 1',
+    component: Day1,
   },
   {
-    path: "/about",
-    name: "About",
+    path: '/day-2',
+    name: 'Day 2',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: 'day-1' */ '../pages/Day-2.vue'),
   },
 ];
 
