bb813951f7b111b55209e8e7c4bcae69d8420aab
Phuc Luong <phuc@inspectorio.com>
2021-12-22 23:04:17 +0700

    up

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular';
+export class AppComponent implements OnInit {
+  public title = 'angular';
+
+  public ngOnInit(): void {
+    const x = {
+      a: 1,
+    };
+  }
 }
