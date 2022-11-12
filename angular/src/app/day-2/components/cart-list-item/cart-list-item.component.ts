1155ad31c194fde492e904136a188705a0388a18
Phuc Luong <luongvanphuc.hcmus@gmail.com>
2022-11-12 09:52:27 +0700

    angular day 3

--- angular/src/app/day-2/components/cart-list-item/cart-list-item.component.ts
+++ angular/src/app/day-2/components/cart-list-item/cart-list-item.component.ts
@@ -1,11 +1,10 @@
-import { Component, ChangeDetectionStrategy, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CartItem } from '../../models';
 
 @Component({
   selector: 'app-cart-list-item',
   templateUrl: './cart-list-item.component.html',
-  styleUrls: ['./cart-list-item.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./cart-list-item.component.scss']
 })
 export class CartListItemComponent {
   @Input()
--- angular/src/app/day-3/day-3.component.ts
+++ angular/src/app/day-3/day-3.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, QueryList, ViewChildren } from '@angular/core';
 import { AudioComponent } from './audio/audio.component';
 
@@ -6,15 +7,16 @@ import { AudioComponent } from './audio/audio.component';
   selector: 'app-day-3',
   templateUrl: './day-3.component.html',
   styleUrls: ['./day-3.component.scss'],
-  imports: [AudioComponent],
+  imports: [CommonModule, AudioComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Day3Component {
   @ViewChildren(AudioComponent)
   public audioPlayers!: QueryList<AudioComponent>;
 
+  public arr23 = Array.from(Array(23).keys());
+
   public play(idx: number): void {
-    const player = this.audioPlayers.get(idx - 1);
-    player?.play();
+    this.audioPlayers.get(idx - 1)?.play();
   }
 }
